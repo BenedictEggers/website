15b407a7fd27853d229fc43d48ee8ef878c43382
Ben Eggers <beggerss@cs.washington.edu>
2015-08-05 12:03:07 -0700

    publish should work from any directory

--- web/scripts/publish.sh
+++ web/scripts/publish.sh
@@ -3,7 +3,9 @@
 # This script will rebuild the site and push it to the server.
 
 SERVER_ADDR="104.131.80.184"
+BASE_DIR="${HOME}/Documents/projects/website/web"
 
+cd ${BASE_DIR}
 ghc --make -threaded site.hs
 ./site rebuild
 scp -r _site/* root@${SERVER_ADDR}:/var/www/html/    # praise SSH keys
