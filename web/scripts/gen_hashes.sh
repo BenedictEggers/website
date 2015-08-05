0cd3fe2fbd9939dafe2c0e3bf85537fd3d3c5737
Ben Eggers <beggerss@cs.washington.edu>
2015-08-05 12:35:03 -0700

    gen_hashes works

--- web/scripts/gen_hashes.sh
+++ web/scripts/gen_hashes.sh
@@ -42,24 +42,43 @@
 # |   |   |   |
 # |   |   |   |-- file4.whoCares
 # |
-# | dest/
-# |  |
-# |  |-- source/
-# |  |   |
-# |  |   |-- sub1
-# |  |   |   |
-# |  |   |   |-- file1.hash
-# |  |   |   |-- file2.hash
-# |  |   |
-# |  |   |-- sub2
-# |  |   |   |
-# |  |   |   |-- file3.hash
-# |  |   |   |-- sub3
-# |  |   |   |   |
-# |  |   |   |   |-- file4.hash
+# |-- dest/
+# |   |
+# |   |-- source/
+# |   |   |
+# |   |   |-- sub1
+# |   |   |   |
+# |   |   |   |-- file1.something.hash
+# |   |   |   |-- file2.whatever.hash
+# |   |   |
+# |   |   |-- sub2
+# |   |   |   |
+# |   |   |   |-- file3.otherThing.hash
+# |   |   |   |-- sub3
+# |   |   |   |   |
+# |   |   |   |   |-- file4.whoCares.hash
 #
 #
 # You MUST make sure that the source and destination do not contain each other.
 # No promises on what happens in that case.
 
+if [[ $# -ne 2 ]]; then
+        echo "Usage: ./gen_hashes.sh <source> <dest>"
+fi
 
+mkdir -p $2
+
+for THING in $( ls $1 ); do
+        if [[ -d $1/$THING ]]; then
+                # directory
+                ./$0 $1/$THING $2/$THING
+
+        elif [[ -f $1/$THING ]]; then
+                # file
+                md5sum $1/$THING > $2/$THING.hash
+        else
+                # ????
+                echo "Error processing ${THING}, quitting"
+                exit 3
+        fi
+done
