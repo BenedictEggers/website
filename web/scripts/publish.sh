3e45321d3bbbf09b26fe7041f372c6a855ceab36
Ben Eggers <beggerss@cs.washington.edu>
2015-08-06 08:42:52 -0700

    publish now uses sshfs and works properly

--- web/scripts/publish.sh
+++ web/scripts/publish.sh
@@ -4,6 +4,8 @@
 
 SERVER_ADDR="104.131.80.184"
 BASE_DIR="${HOME}/Documents/projects/website/web"
+SITE_DIR="_site"
+SERVER_BASE_DIR="/var/www/html"
 
 
 # Remake the site
@@ -12,12 +14,38 @@ ghc --make -threaded site.hs
 ./site rebuild
 
 
-# Remake the hashes
-./scripts/gen_hashes.sh _site hashes/
-ssh root@${SERVER_ADDR} '~/gen_hashes.sh /var/www/html ~/hashes'
+# Mount the remote site
+MOUNT_DIR=$(mktemp -d)
+sshfs -o IdentityFile=/home/ben/.ssh/id_rsa root@${SERVER_ADDR}:${SERVER_BASE_DIR} ${MOUNT_DIR}
 
 
-# Get the remote hashes
-scp -r root@${SERVER_ADDR}:~/hashes remote_hashes/
+# Find differences and fix them
+diff -r ${SITE_DIR} ${MOUNT_DIR} | while read LINE; do
+        if [[ $LINE == diff* ]]; then
+                # Two files differ--copy the _site/ one to ${MOUNT_DIR}
+                WORDS=($LINE)
+                cp ${WORDS[2]} ${WORDS[3]}
 
-scp -r _site/* root@${SERVER_ADDR}:/var/www/html/
+        elif [[ $LINE == Only* ]]; then
+                # We need to either add a file to the server, or delete one from it
+                WORDS=($LINE)
+
+                if [[ ${WORDS[2]} == $SITE_DIR* ]]; then
+                        # Need to add the file to the server
+                        TRUNC=${WORDS[2]:0:-1}
+                        cp ${TRUNC}/${WORDS[3]} ${MOUNT_DIR}/${TRUNC#*/}/${WORDS[3]}
+
+                elif [[ ${WORDS[2]} == $MOUNT_DIR* ]]; then
+                        # Need to delete the file from the server
+                        rm ${WORDS[2]:0:-1}/${WORDS[3]}
+
+                else
+                        echo "Something is horrible broken with $LINE"
+                fi
+        fi
+done
+
+
+# Unmount the server, and delete the directory
+fusermount -u ${MOUNT_DIR}
+rmdir ${MOUNT_DIR}
